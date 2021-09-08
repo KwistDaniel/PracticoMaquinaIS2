529c76dc208b8023c68ad674f436ccdf9e2eddf7
Daniel Kwist <kwistdaniel@gmail.com>
2021-09-08 07:41:02 +0100

    Dependencias y prueba DB

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -5,6 +5,11 @@
  */
 package Main;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  *
  * @author kwist
@@ -16,6 +21,30 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        //jdbc:mariadb://
+        String url = "jdbc:mariadb://dvnikst.duckdns.org:3306/is2-daquino-gk";
+        String user = "user1";
+        String password = "951753";
+        Connection connection = null;
+        System.out.println("ASD");
+        try {
+            //Class.forName("org.mariadb.jdbc.Driver");
+            connection = DriverManager.getConnection(url, user, password);
+            System.out.println("Connected to the PostgreSQL server successfully.");
+            Statement stmt = connection.createStatement();
+            String sql = "CREATE TABLE dfsgsddgsg (id INTEGER)";
+            stmt.executeUpdate(sql);
+            System.out.println("Query tirada");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        try {
+            connection.close();
+            System.out.println("Disconnected to the PostgreSQL server successfully.");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
     
 }
