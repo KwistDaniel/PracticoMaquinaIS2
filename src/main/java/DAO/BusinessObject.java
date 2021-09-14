89bdfdb97d846179eef4a6583d7f87be04cb1a8f
Daniel Kwist <kwistdaniel@gmail.com>
2021-09-14 18:47:50 +0100

    cambios menores

---
+++ src/main/java/DAO/BusinessObject.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+import java.util.List;
+/**
+ *
+ * @author kwist
+ */
+public interface BusinessObject<T> {
+    List<T> readAll();
+    
+    T readOne(String... ids);
+    
+    int create(T t);
+    
+    int update(T t);
+    
+    int delete(T t);
+}
---
+++ src/main/java/Utils/DBQueries.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Utils;
+import DataBase.DataBase;
+
+
+
+/**
+ *
+ * @author kwist
+ */
+public class DBQueries {
+    public static void createAllTables(){
+        //aca llamo las otras queries
+    }
+    
+    public static void tablaCliente(){
+        
+    }
+    
+}
