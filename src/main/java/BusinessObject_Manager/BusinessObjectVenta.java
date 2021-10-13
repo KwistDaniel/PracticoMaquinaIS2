cd6c536c5e73022e07d973f99e8e2ee2cca2af92
Daniel Kwist <kwistdaniel@gmail.com>
2021-10-13 23:43:26 +0100

    cambios

---
+++ src/main/java/BusinessObject_Manager/BusinessObjectVenta.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BusinessObject_Manager;
+
+/**
+ *
+ * @author kwist
+ */
+public class BusinessObjectVenta {
+    
+}
